d9add6d9a0d6e4294b994f6439a49c99b14fd2fb
Jazz <jazzzz@gmail.com>
2019-05-02 20:54:41 +0200

    Homogenize indentation

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
-chrome.browserAction.onClicked.addListener(function(tab){
-    chrome.tabs.getSelected(null, function(tab){
-        var param = {};
-        chrome.tabs.sendRequest(tab.id, param, function(response){
-            //console.log("password view: complete");
-        });
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.getSelected(null, function(tab) {
+    var param = {};
+    chrome.tabs.sendRequest(tab.id, param, function(response) {
+      //console.log("password view: complete");
     });
+  });
 });
